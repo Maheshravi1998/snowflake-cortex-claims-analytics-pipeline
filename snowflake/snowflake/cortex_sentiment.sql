3bdbc39557fece71730e1078fa09f869aee68ca4
Mahesh Ravi <maheshde567@gmail.com>
2026-06-23 11:18:27 -0400

    Create cortex_sentiment.sql
    Cortex_sentiment file

---
+++ snowflake/snowflake/cortex_sentiment.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE TABLE INSURANCE_ANALYTICS_DB.MARTS.CLAIMS_SENTIMENT AS
+SELECT
+    claim_id,
+    policy_id,
+    customer_id,
+    claim_date,
+    claim_type,
+    claim_amount,
+    claim_note,
+    state,
+    SNOWFLAKE.CORTEX.SENTIMENT(claim_note) AS sentiment_score,
+    CASE
+        WHEN SNOWFLAKE.CORTEX.SENTIMENT(claim_note) > 0.3 THEN 'Positive'
+        WHEN SNOWFLAKE.CORTEX.SENTIMENT(claim_note) < -0.3 THEN 'Negative'
+        ELSE 'Neutral'
+    END AS sentiment_category
+FROM INSURANCE_ANALYTICS_DB.RAW.CLAIMS_RAW;
